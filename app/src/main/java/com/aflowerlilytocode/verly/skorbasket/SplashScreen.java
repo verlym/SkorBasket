590979a3130bf337db2d15396b74f525eaf0c128
verlym <muhammadverly@ymail.com>
2017-09-08 16:25:58 +0700

    update icon

---
+++ app/src/main/java/com/aflowerlilytocode/verly/skorbasket/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.aflowerlilytocode.verly.skorbasket;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int jedaSplash = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+
+                this.finish();
+            }
+            private void finish(){
+
+            }
+        },jedaSplash);
+    }
+}
